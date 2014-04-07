e5a7267a575f5050151ab8c63570e9d1a6a6b6c2
Stewart Bryant <stewart@g3ysx.org.uk>
2014-04-08 00:34:33 +0100

    Got atten working via regexp and lots of casting

--- sandpit/paramsand.py
+++ sandpit/paramsand.py
@@ -1,11 +1,22 @@
 import serial
 import matplotlib.pyplot as plt
 import numpy as np
+import re
+
+def getATT(ser):
+    ser.write(b'ATT?\r\n')
+    s = str(ser.read(20).decode('utf8'))
+    print('1',s)
+    s = re.sub('[\x11\x13\r\n AT]','',s)
+    print ('2',s)
+    s = 'ATT:' + str(int(float(s)))
+    print(s)
+    return s
+
 
 ser = serial.Serial('COM10', 9600)
-print(str(ser) + '\n')
 
-print('--', getATT(ser), '--\n')
+print('--' + getATT(ser) + '--\n')
 
 #Ref = REF? then decode hdr for units
 #/div = 0:10, 1:5, 2:2, 3:1 WARNING when peak is on or multi marker mult double db
@@ -18,6 +29,4 @@ print('--', getATT(ser), '--\n')
 #VBW = VBW?
 #SWP = SWP?
 
-def getATT(ser):
-    ser.write(b'ATT?\r\n')
-    return(ser.read(20,1))
+
