a3b51e36927ea3a349a17e1aae2ab59a5e1593d7
Stewart Bryant <stewart@g3ysx.org.uk>
2014-04-08 08:53:50 +0100

    Ref Level Code added

--- sandpit/paramsand.py
+++ sandpit/paramsand.py
@@ -5,22 +5,48 @@ import re
 
 def getATT(ser):
     ser.write(b'ATT?\r\n')
-    s = str(ser.read(20).decode('utf8'))
+    s = str(ser.read(30).decode('utf8'))
     print('1',s)
     s = re.sub('[\x11\x13\r\n AT]','',s)
     print ('2',s)
-    s = 'ATT:' + str(int(float(s)))
+    s = 'ATT:' + str(int(float(s))) + 'dB'
     print(s)
     return s
 
+def getREF(ser):
+    ser.write(b'REF?\r\n')
+    s = str(ser.read(30).decode('utf8'))
+    print('1',s)
+    s = re.sub('[\x11\x13\r\n R]','',s)
+    if s[1] == 'B':
+        u = ' dBm'
+    elif s[1] == 'M':
+        u = ' dBmV'
+    elif s[1] == 'U':
+        u = ' dBuV'
+    elif s[1] == 'E':
+        u = ' dBuVemf'
+    elif s[1] == 'P':
+        u = ' dBpW'
+    elif s[1] == 'V':
+        u = ' V'
+    elif s[1] == 'W':
+        u = ' W'
+    else:
+        u = 'UNDEFINED'
+    print('units = ', u)
+    s = s.replace(s[:2], '')
+    print('2', s)
+    s = 'REF:' + str(float(s)) + u
+    return s
 
 ser = serial.Serial('COM10', 9600)
 
 print('--' + getATT(ser) + '--\n')
+print('--' + getREF(ser) + '--\n')
 
-#Ref = REF? then decode hdr for units
+
 #/div = 0:10, 1:5, 2:2, 3:1 WARNING when peak is on or multi marker mult double db
-#Atten = ATT?
 #start = SRT?
 #Stop = STP?
 #Center = CF
