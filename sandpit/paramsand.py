16b1c448dcce0e03a5571c67892ca467c284b913
Stewart Bryant <stewart@g3ysx.org.uk>
2014-04-09 22:51:01 +0100

    Finished using procs to get params

--- sandpit/paramsand.py
+++ sandpit/paramsand.py
@@ -4,42 +4,34 @@ import numpy as np
 import re
 
 def getParam(ser,paramStr,regStr):
-    ser.write(paramStr.encode('utf8'))
-    s = str(ser.read(40).decode('utf8'))
-    s = re.sub('[\x11\x13\r\n '+ regStr+ ']', '', s)
+    ser.write((paramStr + '?\r\n').encode('utf8'))
+    s = ser.read(40).decode('utf8')
+    s = re.sub('[\x11\x13\r\n ' + regStr + ']', '', s)
     return s
 
 def getATT(ser):
-    #ser.write(b'ATT?\r\n')
-    #s = str(ser.read(30).decode('utf8'))
-    #s = re.sub('[\x11\x13\r\n AT]','',s)
-    s = 'Atten:' + str(int(float(getParam(ser, 'ATT?', 'AT')))) + 'dB'
+    s = 'Atten:' + str(int(float(getParam(ser, 'ATT', 'AT')))) + 'dB'
     return s
 
 def getREF(ser):
-    ser.write(b'REF?\r\n')
-    s = str(ser.read(30).decode('utf8'))
-    print('1',s)
-    s = re.sub('[\x11\x13\r\n R]','',s)
+    s = getParam(ser, 'REF', 'R')
     if s[1] == 'B':
-        u = ' dBm'
+        u = 'dBm'
     elif s[1] == 'M':
-        u = ' dBmV'
+        u = 'dBmV'
     elif s[1] == 'U':
-        u = ' dBuV'
+        u = 'dBuV'
     elif s[1] == 'E':
-        u = ' dBuVemf'
+        u = 'dBuVemf'
     elif s[1] == 'P':
-        u = ' dBpW'
+        u = 'dBpW'
     elif s[1] == 'V':
-        u = ' V'
+        u = 'V'
     elif s[1] == 'W':
-        u = ' W'
+        u = 'W'
     else:
         u = 'UNDEFINED'
-    print('units = ', u)
     s = s.replace(s[:2], '')
-    print('2', s)
     s = 'Reference:' + str(float(s)) + u
     return s
 
@@ -59,92 +51,75 @@ def f2fu(s):
     return str(f)+u
 
 def getSRT(ser):
-    ser.write(b'SRT?\r\n')
-    s = str(ser.read(40).decode('utf8'))
-    print('1',s)
-    s = re.sub('[\x11\x13\r\n FA]','',s)
-    print ('2',s)    
+    s = getParam(ser, 'SRT', 'FA')   
     s = 'Start:' + f2fu(s)
-    print(s)
     return s
 
 def getSTP(ser):
-    ser.write(b'STP?\r\n')
-    s = str(ser.read(40).decode('utf8'))
-    print('1',s)
-    s = re.sub('[\x11\x13\r\n FB]','',s)
-    print ('2',s)    
+    s = getParam(ser, 'STP', 'FB')       
     s = 'Stop:' + f2fu(s)
-    print(s)
     return s
 
 def getCF(ser):
-    ser.write(b'CF?\r\n')
-    s = str(ser.read(40).decode('utf8'))
-    print('1',s)
-    s = re.sub('[\x11\x13\r\n CF]','',s)
-    print ('2',s)    
+    s = getParam(ser, 'CF', 'CF')       
     s = 'Center:' + f2fu(s)
-    print(s)
     return s
 
 def getSP(ser):
-    ser.write(b'SP?\r\n')
-    s = str(ser.read(40).decode('utf8'))
-    print('1',s)
-    s = re.sub('[\x11\x13\r\n SP]','',s)
-    print ('2',s)    
+    s = getParam(ser, 'SP', 'SP')     
     s = 'Span:' + f2fu(s)
-    print(s)
     return s
 
 def getRBW(ser):
-    ser.write(b'RBW?\r\n')
-    s = str(ser.read(40).decode('utf8'))
-    print('1',s)
-    s = re.sub('[\x11\x13\r\n RB]','',s)
-    print ('2',s)    
+    s = getParam(ser, 'RBW', 'RB')       
     s = 'RBW:' + f2fu(s)
-    print(s)
     return s
 
 def getVBW(ser):
-    ser.write(b'VBW?\r\n')
-    s = str(ser.read(40).decode('utf8'))
-    print('1',s)
-    s = re.sub('[\x11\x13\r\n VB]','',s)
-    print ('2',s)    
+    s = getParam(ser, 'VBW', 'VB')     
     s = 'VBW:' + f2fu(s)
-    print(s)
     return s
 
 def getSWP(ser):
-    ser.write(b'SWP?\r\n')
-    s = str(ser.read(40).decode('utf8'))
-    print('1',s)
-    s = re.sub('[\x11\x13\r\n SW]','',s)
-    print ('2',s)
+    s = getParam(ser, 'SWP', 'SW')   
     sw = float(s)
     if sw >= 1:
         return 'Sweep:' + str(sw) + 's'
     else:
         return 'Sweep:' + str(sw*1000) + 'ms'
 
+def getDIV(ser):
+    su = getParam(ser, 'UNIT', '')
+    su = int(su)
+    if su==6 or su==7:
+        return 'LIN'
+    s = getParam(ser, 'DIV', '')
+    s = int(s)
+    if s==0:
+        return '10dB/'
+    if s==1:
+        return '5dB/'
+    if s==2:
+        return '2db/'
+    if s==3:
+        return '1db/'
+    return 'UNKNOWN'
+
 
 ser = serial.Serial('COM10', 9600)
 
-print('--' + getATT(ser) + '--\n')
-print('--' + getREF(ser) + '--\n')
-print('--' + getSRT(ser) + '--\n')
-print('--' + getSTP(ser) + '--\n')
-print('--' + getCF(ser) + '--\n')
-print('--' + getSP(ser) + '--\n')
-print('--' + getRBW(ser) + '--\n')
-print('--' + getVBW(ser) + '--\n')
-print('--' + getSWP(ser) + '--\n')
+print('--' + getATT(ser) + '--')
+print('--' + getREF(ser) + '--')
+print('--' + getSRT(ser) + '--')
+print('--' + getSTP(ser) + '--')
+print('--' + getCF(ser) + '--')
+print('--' + getSP(ser) + '--')
+print('--' + getRBW(ser) + '--')
+print('--' + getVBW(ser) + '--')
+print('--' + getSWP(ser) + '--')
+print('--' + getDIV(ser) + '--')
 
 
 #/div = 0:10, 1:5, 2:2, 3:1 WARNING when peak is on or multi marker mult double db
-
-
+##need the date
 
